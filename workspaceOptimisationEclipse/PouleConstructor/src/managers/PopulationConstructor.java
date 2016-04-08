bf5592725535068bb7c7442804b7d82f528ac6e6
Minipera <olivierperardelle@gmail.com>
2016-04-08 19:11:58 +0200

    Mise en place de quelques fonction en commentaire (non fonctionnel) désolé pour le code dégueux je suis dans le train et j'ai pas pu pull avant de faire mes modifs donc il me manques des éléments, aller @+

--- workspaceOptimisationEclipse/PouleConstructor/src/managers/PopulationConstructor.java
+++ workspaceOptimisationEclipse/PouleConstructor/src/managers/PopulationConstructor.java
@@ -10,6 +10,66 @@ import object.Poule;
 public class PopulationConstructor {
 
 	public PopulationConstructor() {
+		
+		/*
+		 * for (int i in 1 to config.NombreIndividus)
+		 * {
+		 * 		GenererIndividuAleatoire();
+		 * }
+		 * 
+		 * 
+		 * 
+		 * */
+		
+	}
+	
+	public static Vector<Individu> SelectionMeilleursIndividus() {
+		
+		/* ALGO DEGUEULASSE NIVEAU OPTIMISATION A VOIR SI ON PEUT FAIRE MIEUX
+		 * 
+		 * On regarde sur chaque individu s'il existe un autre individu qui est dominé
+		 * c'est à dire si i.EcartDeNiveau < j.EcarteDeNiveau ET i.distanceTotale < j.distanceTotale
+		 *   
+		 * 		Vector<Individus> Population;
+		 *  Population = ObjectManager.getMesIndividus()
+		 *     for (individu i in population)
+		 *     {
+		 *     		for (individu j in population)
+		 *     		{
+		 *     // TODO : Cas ou on choisi la durée comme critère dans le fichier de config
+		 *     			if ((i.EcartDeNiveau < j.EcartDeNiveau) && (i.DistanceTotal < j.distanceTotale))
+		 *     			{
+		 *     				// TODO: ICI IL FAUT SUPPRIMER J DU VECTEUR POPULATION
+		 *     			}
+		 *     		}
+		 *     }
+		 * 		
+		 * 		if (is.empty(Population))
+		 * 	{
+		 * 		System.out.println("Tous les individus sont dominés par un autre, 
+		 * soit solution optimale soit critère trop strict !\n");
+		 * 	}
+		 * 
+		 * return Population
+		 * 
+		 * */
+		Vector<Individu> Population = null;
+		return Population;
+	}
+	// On fait muter l'individu, on l'ajoute a la population, et on selectionne les N meilleurs
+	public static void mutationPopulation(Vector<Individu> Population)
+	{
+		for (Individu i : Population)
+		{
+			//TODO: créer la fonction dans classe Individu
+			//i.mutation()
+			
+		}
+		// On ajoute les mutants dans la population
+		// PopulationGlobale ?? .ajoutePopulation(Population);
+		// PopulationGlobale ?? . triMeilleursIndividus();
+		
+
 	}
 	
 	public static void GenererIndividuAleatoire() {
